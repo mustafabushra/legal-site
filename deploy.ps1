50be641d6a68cc6dcb70a394435065f742bb45ac
mustafabushra <mustafabushra1779@gmail.com>
2026-05-21 06:15:22 +0300

    fix: deploy script no bash history expansion

--- deploy.ps1
+++ deploy.ps1
@@ -7,10 +7,7 @@ git commit -m $msg
 git push render master
 
 Write-Host "Deploying to EC2..." -ForegroundColor Cyan
-$cmd = @"
-cd alhusseinalsaadi.sa && git pull && docker build -t legal-site . && docker stop legal-site; docker rm legal-site; docker run -d --name legal-site --restart always -p 3000:3000 -v /data:/data --env-file .env.production legal-site && echo 'Deploy done!'
-"@
+ssh -i "C:\Users\DTG\legal-site\key.pem" ubuntu@16.171.113.27 "bash ~/deploy.sh"
 
-ssh -i "C:\Users\DTG\legal-site\key.pem" ubuntu@16.171.113.27 $cmd
 gh auth switch --user mustafabushra | Out-Null
 Write-Host "Done! https://alhusseinalsaadi.sa" -ForegroundColor Green
